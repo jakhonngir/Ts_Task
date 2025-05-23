6e91bf0903bac7cb3c3617682306a7fb6661edaf
jakhonngir <bahromovam843@gmail.com>
2025-05-23 22:19:18 +0500

    arraydagi bir xil sonlarni 1ta qoldirish

--- app.ts
+++ app.ts
@@ -42,92 +42,92 @@
 // const upperCaseCars = cars.map(car => car.toUpperCase())
 // console.log(upperCaseCars);
 
-const h1 = document.querySelector('.h1') as HTMLElement;
-const h2 = document.querySelector('.h2') as HTMLElement;
-const h3 = document.querySelector('.h3') as HTMLElement;
-const h4 = document.querySelector('.h4') as HTMLElement;
+// const h1 = document.querySelector('.h1') as HTMLElement;
+// const h2 = document.querySelector('.h2') as HTMLElement;
+// const h3 = document.querySelector('.h3') as HTMLElement;
+// const h4 = document.querySelector('.h4') as HTMLElement;
 
 
-enum StudentStatus {
-  Active = 'Active',
-  Graduated = 'Graduated',
-  Dropped = 'Dropped'
-}
+// enum StudentStatus {
+//   Active = 'Active',
+//   Graduated = 'Graduated',
+//   Dropped = 'Dropped'
+// }
 
-interface Student {
-  id: number;
-  name: string;
-  age: number;
-  status: StudentStatus;
-  grades: number[];
-}
+// interface Student {
+//   id: number;
+//   name: string;
+//   age: number;
+//   status: StudentStatus;
+//   grades: number[];
+// }
 
-const students: Student[] = [
-  {
-    id: 1,
-    name: "Ali",
-    age: 20,
-    status: StudentStatus.Active,
-    grades: [80, 75, 90]
-  },
-  {
-    id: 2,
-    name: "Laylo",
-    age: 17,
-    status: StudentStatus.Graduated,
-    grades: [88, 92, 95]
-  },
-  {
-    id: 3,
-    name: "Diyor",
-    age: 22,
-    status: StudentStatus.Dropped,
-    grades: [60, 55, 70]
-  },
-  {
-    id: 4,
-    name: "Malika",
-    age: 19,
-    status: StudentStatus.Active,
-    grades: [100, 95, 98]
-  }
-];
+// const students: Student[] = [
+//   {
+//     id: 1,
+//     name: "Ali",
+//     age: 20,
+//     status: StudentStatus.Active,
+//     grades: [80, 75, 90]
+//   },
+//   {
+//     id: 2,
+//     name: "Laylo",
+//     age: 17,
+//     status: StudentStatus.Graduated,
+//     grades: [88, 92, 95]
+//   },
+//   {
+//     id: 3,
+//     name: "Diyor",
+//     age: 22,
+//     status: StudentStatus.Dropped,
+//     grades: [60, 55, 70]
+//   },
+//   {
+//     id: 4,
+//     name: "Malika",
+//     age: 19,
+//     status: StudentStatus.Active,
+//     grades: [100, 95, 98]
+//   }
+// ];
 
-for (let i = 0; i < students.length; i++) {
-  var studentsName: string = students[i].name;
-  var studentsAge: number = students[i].age;
-  var studentsStatus = students[i].status;
-  var studentsGrades = students[i].grades;
+// for (let i = 0; i < students.length; i++) {
+//   var studentsName: string = students[i].name;
+//   var studentsAge: number = students[i].age;
+//   var studentsStatus = students[i].status;
+//   var studentsGrades = students[i].grades;
 
 
 
-  if (studentsAge > 18) {
-    console.log(`18 yoshdan kattalar ismi: ${studentsName}; yoshi: ${studentsAge}`);
-  }
-  if (studentsStatus == StudentStatus.Active) {
-    console.log(`Active: ${studentsName}`);
-  }
+//   if (studentsAge > 18) {
+//     console.log(`18 yoshdan kattalar ismi: ${studentsName}; yoshi: ${studentsAge}`);
+//   }
+//   if (studentsStatus == StudentStatus.Active) {
+//     console.log(`Active: ${studentsName}`);
+//   }
 
-  for (let index = 0; index < studentsGrades.length; index++) {
+//   for (let index = 0; index < studentsGrades.length; index++) {
 
-    const min = Math.min(...studentsGrades);
-    const max = Math.max(...studentsGrades);
+//     const min = Math.min(...studentsGrades);
+//     const max = Math.max(...studentsGrades);
 
-    let indexMax = studentsGrades.indexOf(max);
-    if (indexMax !== -1) {
-      studentsGrades.splice(indexMax, 1);
-    }
+//     let indexMax = studentsGrades.indexOf(max);
+//     if (indexMax !== -1) {
+//       studentsGrades.splice(indexMax, 1);
+//     }
 
-    let indexMin = studentsGrades.indexOf(min);
-    if (indexMin !== -1) {
-      studentsGrades.splice(indexMin, 1);
-    }
-        
-    console.log(`${studentsName}ning o'rtacha bali ${studentsGrades}`);
-    
-  }
+//     let indexMin = studentsGrades.indexOf(min);
+//     if (indexMin !== -1) {
+//       studentsGrades.splice(indexMin, 1);
+//     }
 
-}
+//     console.log(`${studentsName}ning o'rtacha bali ${studentsGrades}`);
+
+//   }
+
+// }
 
 // Talabalar ro‘yxati yaratiladi
 
@@ -143,3 +143,25 @@ for (let i = 0; i < students.length; i++) {
 // Har bir talaba uchun o‘rtacha baho hisoblanadi
 
 // Qisqa StudentSummary[] tuziladi
+
+
+
+const arr: number[] = [
+  42, 3, 60, 7, 19, 85, 25, 42, 64, 76,
+  1, 99, 31, 17, 27, 33, 17, 49, 57, 4,
+  2, 5, 8, 9, 10, 11, 12, 13, 14, 15,
+  16, 18, 20, 21, 22, 23, 24, 26, 28, 29,
+  30, 32, 34, 35, 36, 37, 38, 39, 40, 41,
+  43, 44, 45, 46, 47, 48, 50, 51, 52, 53,
+  54, 55, 56, 58, 59, 60, 61, 62, 63, 65,
+  66, 67, 68, 69, 70, 71, 72, 73, 74, 75,
+  77, 78, 79, 80, 81, 82, 83, 84, 85, 86,
+  87, 88, 89, 90, 91, 92, 93, 94, 95, 96
+];
+
+  console.log(arr);
+  
+  const uniqueNumbers = Array.from(new Set(arr));
+  console.log(uniqueNumbers);
+
+// bir xil sonlarni uchirib bita qoldirish uchun method
